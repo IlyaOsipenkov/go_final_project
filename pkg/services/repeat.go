59c2efd786a36385d9c720987df0e5438bb94b2e
IlyaOsipenkov <dakarlog@gmail.com>
2025-01-22 04:12:17 +0300

    case_m

--- pkg/services/repeat.go
+++ pkg/services/repeat.go
@@ -18,17 +18,20 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 	if len(repeat) == 0 {
 		fmt.Println("delete task from db")
 	} else {
-		cleanedRepeat := strings.ReplaceAll(repeat, ",", "")
-		arrOfRuleAndDates := strings.Split(cleanedRepeat, " ")
+
+		// cleanedRepeat := strings.ReplaceAll(repeat, ",", "")
+		// arrOfRuleAndDates := strings.Split(cleanedRepeat, " ")
+		// rule := arrOfRuleAndDates[0]
+		// daysOfRule := arrOfRuleAndDates[1:]
+		arrOfRuleAndDates := strings.Fields(repeat)
 		rule := arrOfRuleAndDates[0]
-		daysOfRule := arrOfRuleAndDates[1:]
 
 		switch rule {
 		case "d":
-			if len(arrOfRuleAndDates) < 2 {
+			if len(arrOfRuleAndDates) != 2 {
 				return "", errors.New("repeat doesnt valid for rule -d-")
 			}
-			days, err := strconv.Atoi(daysOfRule[0])
+			days, err := strconv.Atoi(arrOfRuleAndDates[1])
 			if err != nil || days < 0 || days < 400 {
 				return "", errors.New("days of repead doesnt valid. Must be in 1-400")
 			}
@@ -49,7 +52,7 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 			if len(arrOfRuleAndDates) != 2 {
 				return "", errors.New("repeat doesnt valid for rule -w-")
 			}
-			daysOfWeek, err := parseDaysOfWeek(daysOfRule)
+			daysOfWeek, err := parseDaysOfWeek(arrOfRuleAndDates[1])
 			if err != nil {
 				return "", err
 			}
@@ -57,18 +60,17 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 			return nextDate.Format("20060102"), nil
 
 		case "m":
-
+			//переписал разбиение repeat, дописать кейс
 		default:
 			fmt.Println("wrong rule")
 		}
 	}
-	//написать сплит repeat и проверку на 2 часть
 }
 
-func parseDaysOfWeek(data []string) ([]int, error) {
+func parseDaysOfWeek(data string) ([]int, error) {
+	days := strings.Split(data, ",")
 	weekdays := []int{}
-
-	for _, val := range data {
+	for _, val := range days {
 		day, err := strconv.Atoi(val)
 		if err != nil || day < 1 || day > 7 {
 			return nil, fmt.Errorf("the day doesnt valid: %s", val)
